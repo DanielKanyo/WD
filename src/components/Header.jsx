ea4f10f218005219a9549757930098e8ca5cf17e
DanielKanyo <danielkanyo992@gmail.com>
2017-11-05 14:32:00 +0100

    content image, design, header component changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,56 +40,57 @@ class Header extends React.Component {
 
     return (
       <div id="header-container">
-        <div className="title">Work Diary!</div>
-        <div className="menu">
-          {/* LOGIN BUTTON */}
-          <button className="menuBtn LoginBtn" onClick={function () {
-            this.handleUpdateIndexView(4);
-            this.changeClass('login')
-          }.bind(this)
-          }>
-            <TumblrReblogIcon className={"menuBtnIcon loginIco " + selectedIcon["login"]} />
-            <div className={"menuBtnText " + selectedIcon["login"]}>Login</div>
-          </button>
-          {/* THROWBACK BUTTON */}
-          <button className="menuBtn ThrowbackBtn" onClick={function () {
-            this.handleUpdateIndexView(3);
-            this.changeClass('throwback')
-          }.bind(this)
-          }>
-            <CalendarIcon className={"menuBtnIcon throwbackIco " + selectedIcon["throwback"]} />
-            <div className={"menuBtnText " + selectedIcon["throwback"]}>Throwback</div>
-          </button>
-          {/* DIAGRAM BUTTON */}
-          <button className="menuBtn DiagramBtn" onClick={function () {
-            this.handleUpdateIndexView(2);
-            this.changeClass('diagram')
-          }.bind(this)
-          }>
-            <ChartLineIcon className={"menuBtnIcon " + selectedIcon["diagram"]} />
-            <div className={"menuBtnText " + selectedIcon["diagram"]}>Statistics</div>
-          </button>
-          {/* ABOUT BUTTON */}
-          <button className="menuBtn AboutBtn" onClick={function () {
-            this.handleUpdateIndexView(1);
-            this.changeClass('about')
-          }.bind(this)
-          }>
-            <InformationOutlineIcon className={"menuBtnIcon " + selectedIcon["about"]} />
-            <div className={"menuBtnText " + selectedIcon["about"]}>About</div>
-          </button>
-          {/* DIARY BUTTON */}
-          <button className="menuBtn DiaryBtn" onClick={function () {
-            this.handleUpdateIndexView(0);
-            this.changeClass('diary')
-          }.bind(this)
-          }>
-            <BookIcon className={"menuBtnIcon " + selectedIcon["diary"]} />
-            <div className={"menuBtnText " + selectedIcon["diary"]}>Diary</div>
-          </button>
+        <div className="menuBar">
+          <div className="title">Work Diary!</div>
+          <div className="menu">
+            {/* LOGIN BUTTON */}
+            <button className="menuBtn LoginBtn" onClick={function () {
+              this.handleUpdateIndexView(4);
+              this.changeClass('login')
+            }.bind(this)
+            }>
+              <TumblrReblogIcon className={"menuBtnIcon loginIco " + selectedIcon["login"]} />
+              <div className={"menuBtnText " + selectedIcon["login"]}>Login</div>
+            </button>
+            {/* THROWBACK BUTTON */}
+            <button className="menuBtn ThrowbackBtn" onClick={function () {
+              this.handleUpdateIndexView(3);
+              this.changeClass('throwback')
+            }.bind(this)
+            }>
+              <CalendarIcon className={"menuBtnIcon throwbackIco " + selectedIcon["throwback"]} />
+              <div className={"menuBtnText " + selectedIcon["throwback"]}>Throwback</div>
+            </button>
+            {/* DIAGRAM BUTTON */}
+            <button className="menuBtn DiagramBtn" onClick={function () {
+              this.handleUpdateIndexView(2);
+              this.changeClass('diagram')
+            }.bind(this)
+            }>
+              <ChartLineIcon className={"menuBtnIcon " + selectedIcon["diagram"]} />
+              <div className={"menuBtnText " + selectedIcon["diagram"]}>Statistics</div>
+            </button>
+            {/* ABOUT BUTTON */}
+            <button className="menuBtn AboutBtn" onClick={function () {
+              this.handleUpdateIndexView(1);
+              this.changeClass('about')
+            }.bind(this)
+            }>
+              <InformationOutlineIcon className={"menuBtnIcon " + selectedIcon["about"]} />
+              <div className={"menuBtnText " + selectedIcon["about"]}>About</div>
+            </button>
+            {/* DIARY BUTTON */}
+            <button className="menuBtn DiaryBtn" onClick={function () {
+              this.handleUpdateIndexView(0);
+              this.changeClass('diary')
+            }.bind(this)
+            }>
+              <BookIcon className={"menuBtnIcon " + selectedIcon["diary"]} />
+              <div className={"menuBtnText " + selectedIcon["diary"]}>Diary</div>
+            </button>
 
+          </div>
         </div>
-
         <div className="datepicker">DatePicker</div>
 
         <ReactTooltip effect="solid" place="bottom" />
